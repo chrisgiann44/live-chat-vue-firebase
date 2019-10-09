d2634c83f6eb1273659ed2ccc7b8d8ab27b35063
Christos Giannouris <chris1giann1@gmail.com>
2019-10-09 12:07:23 +0200

    Chat Component

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,13 +1,31 @@
 <template>
     <div class="chat container">
-        <h2>Chat {{this.name}}</h2>
+        <h2 class="center teal-text">Your Chat</h2>
+        <div class="card">
+            <div class="card-content">
+                <ul class="messages">
+                    <li>
+                        <span class="teal-text">Name</span>
+                        <span class="grey-text text-darken-3">Message</span>
+                        <span class="grey-text time">Time</span>
+                    </li>
+                </ul>
+            </div>
+            <div class="card-action">
+                <NewMessage :name="name" />
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import NewMessage from "@/components/NewMessage";
 export default {
     name: "Chat",
     props: ["name"],
+    components: {
+        NewMessage
+    },
     data() {
         return {};
     }
@@ -15,4 +33,17 @@ export default {
 </script>
 
 <style scoped>
+.chat h2 {
+    font-size: 2.6em;
+    margin-bottom: 40px;
+}
+
+.chat span {
+    font-size: 1.4em;
+}
+
+.chat .time {
+    display: block;
+    font-size: 1.2em;
+}
 </style>
---
+++ src/components/NewMessage.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="new-message">
+        <form @submit.prevent="addMessage">
+            <label for="new-message">New Message (enter to add):</label>
+            <input type="text" name="new-message" v-model="newMessage" />
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "NewMessage",
+    props: ["name"],
+    data() {
+        return {
+            NewMessage: null
+        };
+    }
+};
+</script>
+
+<style scoped>
+</style>
