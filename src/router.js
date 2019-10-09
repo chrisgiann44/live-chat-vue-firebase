3e5ae3311594ee2ee452837f1775d845a7181dad
Christos Giannouris <chris1giann1@gmail.com>
2019-10-09 11:40:25 +0200

    Chat Component

--- src/router.js
+++ src/router.js
@@ -1,27 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Welcome from '@/components/Welcome'
+import Chat from '@/components/Chat'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: function () { 
-        return import(/* webpackChunkName: "about" */ './views/About.vue')
-      }
+      name: 'Welcome',
+      component: Welcome
+    }, {
+      path: '/chat',
+      name: 'Chat',
+      component: Chat,
+      props: true
     }
   ]
 })
